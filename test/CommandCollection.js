23da3f70f405b09e5bd000741e1c34538884fd78
Howard Tyson <howard@howardtyson.com>
2018-05-30 08:35:13 -0400

    Fixing missing subcommand error handling and adding a test to validate the behavior.

--- test/CommandCollection.js
+++ test/CommandCollection.js
@@ -8,6 +8,32 @@ const Command = Riker.Command;
 const yargs = require('yargs');
 
 describe('CommandCollection', function() {
+  it('should display help if an unknown command is specified', (done) => {
+    const history = [];
+    const options = {
+      args: ['def'],
+      subCommands: {
+        foo: 'bar',
+      },
+      errorStream: {
+        write: (data) => history.push({stream: 'error', data}),
+        end: () => {},
+      },
+      outputStream: {
+        write: (data) => history.push({stream: 'error', data}),
+        end: () => {},
+      }
+    };
+    const collection = new CommandCollection(options);
+    should.not.exist(collection.getSubCommand('def', []));
+    collection.run(() => {
+      history[0].stream.should.equal('error');
+      history[0].data.should.containEql('Invalid sub-command specified');
+      history[0].data.should.containEql('def');
+      done();
+    });
+
+  });
   describe('initialization', () => {
     it('should populate subcommands', () => {
       const options = {
